4835a3058aba07adecc194687d1417070ac0db79
Noah Cote <noahcote10@gmail.com>
2022-10-17 21:42:18 -0400

    created files, sketched outline of barebones code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const span = document.getElementById("word")
+// const word ["javascript", "array", "variable"]
+
+// const word
+// const secretWord += "_";
+
+// word.textcontent = secretWord;
+
+document.addEventListener("keydown", function(e) {
+    console.log();
+    // e.key === word[i]
+    // secretWord[i] = e.key;
+    // word.textContent = secretWord;
+})
